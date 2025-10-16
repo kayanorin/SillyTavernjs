c4a5e990c37e947965869abba97b080841fb3ce6
kayanorin <kayanorin@gmail.com>
2025-10-16 21:54:00 +0800

    脚本字典更新

--- variant-chars-dict.js
+++ variant-chars-dict.js
@@ -1,45 +1,15 @@
-// 全局变量声明 - 确保可以被访问
-window.VARIANT_DICT = {
-    "慊弃": {
-        original: "嫌弃",
-        explanation: "厌恶而不愿接近"
-    },
-    "忮愱": {
-        original: "嫉妒",
-        explanation: "因别人比自己好而憎恨"
-    },
-    "忮忌": {
-        original: "嫉妒",
-        explanation: "嫉妒忌恨"
-    },
-    "倒楣": {
-        original: "倒霉",
-        explanation: "遭遇不顺利"
-    },
-    "秽气": {
-        original: "晦气",
-        explanation: "不吉利、运气不好"
-    },
-    "闝倡": {
-        original: "嫖娼",
-        explanation: "狎妓"
-    },
-    "白剽": {
-        original: "白嫖",
-        explanation: "不付费而获取"
-    },
-    "贪惏": {
-        original: "贪婪",
-        explanation: "贪得无厌"
-    },
-    "虜隶": {
-        original: "奴隶",
-        explanation: "被奴役的人"
-    },
-    "螙药": {
-        original: "毒药",
-        explanation: "有毒的药物"
-    }
-};
-
-console.log('[VariantDict] 字典已加载，共', Object.keys(window.VARIANT_DICT).length, '个词条');
+(function() {
+    window.VARIANT_DICT = {
+        "慊弃": { original: "嫌弃", explanation: "厌恶而不愿接近" },
+        "忮愱": { original: "嫉妒", explanation: "因别人比自己好而憎恨" },
+        "忮忌": { original: "嫉妒", explanation: "嫉妒忌恨" },
+        "倒楣": { original: "倒霉", explanation: "遭遇不顺利" },
+        "秽气": { original: "晦气", explanation: "不吉利、运气不好" },
+        "闝倡": { original: "嫖娼", explanation: "狎妓" },
+        "白剽": { original: "白嫖", explanation: "不付费而获取" },
+        "贪惏": { original: "贪婪", explanation: "贪得无厌" },
+        "虜隶": { original: "奴隶", explanation: "被奴役的人" },
+        "螙药": { original: "毒药", explanation: "有毒的药物" }
+    };
+    console.log('[VariantDict] 字典已加载到 window.VARIANT_DICT');
+})();
